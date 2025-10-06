10a09c4ecde7e5e3b1c382c2d4db656e2fce231c
04arben <b00168185@mytudublin.ie>
2025-10-06 16:00:11 +0100

    First part is done. I made my js code in a way that when submitted it calculates the final result and shows it to the user.

---
+++ week-4/App.js
@@ -0,0 +1,45 @@
+import { SafeAreaView, Text, View, TextInput, Button, Alert, StyleSheet } from 'react-native';
+import {useState} from 'react';
+
+export default function App() {
+  const gradePoints = {'F': 0,'D': 1.5,'C': 2,'C+': 2.75, 'B': 3, 'B+': 3.5,  'A': 4 };
+  
+  const [sswd, setSswd] = useState('D');
+  const [ob, setOb] = useState('D');
+  
+  var gpa=0;
+  var credits=5;
+  var totalPossibleCredits = 10;
+  var totalGradeScores = 0;
+  
+  function clickMe() {
+    alert("this is the click me button"); // alert for web
+    Alert.alert("this is the click me button"); // alert for phone
+
+    
+    totalGradeScores += gradePoints[sswd.toUpperCase()] * credits;
+    totalGradeScores += gradePoints[ob.toUpperCase()] * credits;
+    gpa = totalGradeScores / totalPossibleCredits;
+
+    
+    Alert.alert("Your GPA is " + gpa.toFixed(2));
+  }
+
+
+return (
+    <SafeAreaView>
+      <View><Text style={{flexDirection: "row", fontWeight: "bold", fontSize: 24, textAlign:"center", marginTop: "%10"}}>GPA Calculator</Text></View>
+      <View>
+        <Text>Maths</Text>
+        <TextInput  placeholder="Grade" onChangeText={setSswd}/>
+      </View>
+      <View >
+        <Text>Organisational Behaviour</Text>
+        <TextInput placeholder="Grade" onChangeText={setOb}/>
+      </View>
+      <View >
+        <Button title="submit" onPress={clickMe}/>
+      </View>
+    </SafeAreaView>
+  );
+}
